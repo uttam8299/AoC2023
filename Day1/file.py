30fb70b0d8d86b9147616a537579c59e583cbaf1
uttam8299 <uttam.yadav8299@gmail.com>
2023-12-02 00:53:26 +0530

    2nd

--- Day1/file.py
+++ Day1/file.py
@@ -1,6 +1,45 @@
 
+numDict = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '0': 0
+}
+
+
+
+def convertWordToNum(line):
+    tempDict = {}
+    for key in numDict:
+        idx = line.find(key)
+        if idx > -1:
+            tempDict[key] = idx
+    dicttemp = sorted(tempDict.items(), key=lambda x:x[1])
+    dicttemp = dict(dicttemp)
+    tempstr = ''
+    for key in dicttemp:
+        tempstr += str(numDict[key])
+    return tempstr
+
 # Actual logic goes here
 def calculate(line):
+    line = convertWordToNum(line)
+    print(line)
     i = 0
     j = len(line) - 1
     result = 0
